befb399164098022f01dda296c9b51d940eeb64c
Vitaly Chouliak <vitaly.chouliak@hotmail.com>
2025-06-03 15:20:00 +0200

    Make AudioFileInfo immutable and add JsonConstructor for deserialization
    - Properties are now readonly to enforce immutability
    - Added JsonConstructor to support JSON deserialization
    - This improves code safety and clarity when loading from configuration

--- VirtualNvhAnalyzer.core/Models/AudioFileInfo.cs
+++ VirtualNvhAnalyzer.core/Models/AudioFileInfo.cs
@@ -1,10 +1,21 @@
-﻿namespace VirtualNvhAnalyzer.Core.Models
+﻿using System.Text.Json.Serialization;
+
+namespace VirtualNvhAnalyzer.Core.Models
 {
     public class AudioFileInfo
     {
-        public string FileName { get; set; }
-        public int SampleRate { get; set; }
-        public int Channels { get; set; }
-        public TimeSpan Duration { get; set; }
+        public string FileName { get;}
+        public int SampleRate { get;}
+        public int Channels { get;}
+        public TimeSpan Duration { get;}
+
+        [JsonConstructor]
+        public AudioFileInfo(string fileName, int sampleRate, int channels, TimeSpan duration)
+        {
+            FileName = fileName;
+            SampleRate = sampleRate;
+            Channels = channels;
+            Duration = duration;
+        }
     }
 }
